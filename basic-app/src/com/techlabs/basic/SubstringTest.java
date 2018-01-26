cec82ea50a12243f82fa52929bf51d2081b6faa9
vipul <vipulsawant25@gmail.com>
2018-01-26 23:18:47 +0530

    gitignore works now.

---
+++ basic-app/src/com/techlabs/basic/SubstringTest.java
@@ -0,0 +1,24 @@
+package com.techlabs.basic;
+
+public class SubstringTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String addr = "www.swabhavtechlabs.com?developer=vipul";
+		int a = addr.indexOf(".")+1;
+		int b = addr.lastIndexOf(".");
+		
+		System.out.println("Using substring");
+		System.out.println("Domain name: "+addr.substring(a,b));
+		a = addr.indexOf("=")+1;
+		System.out.println("Dev name: "+addr.substring(a));
+		
+		System.out.println("\nUsing Split");
+		String temp[] = addr.split("\\.");
+		System.out.println("Domain name: "+temp[1]);
+		temp = addr.split("\\=");
+		System.out.println("Dev name: "+temp[1]);
+		
+	}
+
+}
